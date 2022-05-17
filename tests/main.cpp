5dd2f8864a3f2ac02dd3e24794db1227c269ac0b
vantunes-crl <valerio.asj@gmail.com>
2022-05-17 14:25:25 +0100

    doing tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,14 +1,15 @@
 #include "vector_tester.hpp"
 #include "../vector.hpp"
 
-#define VECTOR &std::vector<int>
-#define FT_VECTOR &ft::vector<int>
-
 int main(int argc, char **argv)
 {
     vector_test<int> test;
 
     test.Push_back();
+    test.Resize();
     test.Assign();
+    test.Assign_range();
+    test.Pop_back();
+    test.At();
 
 }
--- tests/vector_tester.hpp
+++ tests/vector_tester.hpp
@@ -4,14 +4,15 @@
 #include <vector>
 #include "../vector.hpp"
 #include <iostream>
-#include "print.hpp"
-#include <chrono>
 #include <string>
 #include <iomanip>
 #include <stdio.h>
-#include <functional>
+#include <fstream>
+#include <sstream>
+#include <streambuf>
 
-
+#define START_TIME _clock = clock();
+#define FINISH_TIME double(clock() - _clock) / double(CLOCKS_PER_SEC);
 #define GREEN "\033[1;32m"
 #define RED "\033[1;31m"
 #define END "\033[0m"
@@ -20,85 +21,56 @@
 template<class T>
 class vector_test
 {
+    typedef struct s_result
+    {
+        double time1;
+        double time2;
+        std::string s1;
+        std::string s2;
+        bool error;
+
+        void clear() 
+        {
+            time1 = 0;
+            time2 = 0;
+            s1.clear();
+            s2.clear();
+            error = false;
+        }
+
+    } t_result;
+
     private:
         std::ofstream log{ "Logs"};
+        t_result _result;
         std::vector<T> _vec;
         ft::vector<T> _ft_vec;
-    public:
-        /************ Constructors ************/
-        vector_test()
+        clock_t _clock;
+
+        void result(std::string name)
         {
-            std::cout << std::setw(7) << "Test" << std::setw(25) << "Diff" << std::setw(13) << "Time" << std::endl;
-        }
-
-        /************ Functions ***************/
-
-        void Push_back()
-        {
-            clock_t start, end;
-
-            //execution time and test vector
-            start = clock();
-            for (int i = 0; i < 500; ++i)
-                _vec.push_back(i);
-            end = clock();
-            double result1 = double(end - start) / double(CLOCKS_PER_SEC);
-
-            start = clock();
-            for (int i = 0; i < 500; ++i)
-                _ft_vec.push_back(i);
-            end = clock();
-            double result2 = double(end - start) / double(CLOCKS_PER_SEC);
-
-            std::string vec1;
-            std::string vec2;
-
-            vec1 = print(_vec);
-            vec2 = print(_ft_vec);
-
-            result(vec1, vec2, result1, result2, "Push_back");
-        }
-
-        void Assign()
-        {
-            clock_t start, end;
-
-            //execution time and test vector
-            start = clock();
-            _vec.assign(100,200);
-            end = clock();
-            double result1 = double(end - start) / double(CLOCKS_PER_SEC);
-
-            start = clock();
-            _ft_vec.assign(100,200);
-            end = clock();
-            double result2 = double(end - start) / double(CLOCKS_PER_SEC);
-
-            std::string vec1;
-            std::string vec2;
-
-            vec1 = print(_vec);
-            vec2 = print(_ft_vec);
-
-            result(vec1, vec2, result1, result2, "Assign");
-        }
-
-        void result(std::string &s1, std::string &s2, double time1, double time2, std::string name)
-        {
-            bool error = false;
-            log << compare(s1, s2, &error);
-            log << name;
-            if (error)
-                printf("%10s%-10s Test%10s %10sError%10s", YELLOW, name.c_str(), END, RED, END);
+            log << compare(_result.s1, _result.s2);
+            log << name << '\n';
+            if (_result.error)
+                printf("%5.10s%-10s %10s %11sError%10s", YELLOW, name.c_str(), END, RED, END);
             else
-                printf("%10s%-10s Test%10s %10sPassed%10s", YELLOW, name.c_str(), END, GREEN, END);
-            if (time2 > (time1 * 3))
-                printf("%s %f %s\n", RED, time2, END);
+                printf("%5.10s%-10s %10s %10sPassed%10s", YELLOW, name.c_str(), END, GREEN, END);
+            if (_result.time2 > (_result.time1 * 3))
+                printf("%s %5f %s", RED, _result.time2, END);
             else
-                printf("%s %f %s\n", GREEN, time2, END);
+                printf("%s %5f %s", GREEN, _result.time2, END);
+            if (_vec.capacity() != _ft_vec.capacity())
+                printf("%s %-10zu %s", RED, _ft_vec.capacity(), END);
+            else
+                printf("%s %-10zu %s", GREEN, _ft_vec.capacity(), END);
+            if (_vec.size() != _ft_vec.size())
+                printf("%s %zu %s\n", RED, _ft_vec.size(), END);
+            else
+                printf("%s %zu %s\n", GREEN, _ft_vec.size(), END);
+            _result.clear();
         } 
 
-        std::string compare(std::string &a, std::string &b, bool *error)
+        std::string compare(std::string &a, std::string &b)
         {
             std::string diff;
             int i = 0;
@@ -111,15 +83,159 @@ class vector_test
                         diff.push_back(a[i]);
                     else
                         diff.push_back(b[i]);
-                    *error = true;
+                    _result.error = true;
                 }
                 ++i;
             }
-            
             return diff;
         }
 
+        void print()
+        {    
+            std::stringstream ss;
+            std::stringstream ss2;
 
+            std::streambuf *old_buf = std::cout.rdbuf(ss.rdbuf());
+
+            for (auto it = _vec.begin(); it != _vec.end(); ++it)
+                std::cout << *it;
+
+            std::cout.rdbuf(old_buf);
+            _result.s1 = ss.str();
+
+            old_buf = std::cout.rdbuf(ss2.rdbuf());
+
+            for (auto it = _ft_vec.begin(); it != _ft_vec.end(); ++it)
+                std::cout << *it;
+
+            std::cout.rdbuf(old_buf);
+            _result.s2 = ss2.str();
+        }
+
+    public:
+        /************ Constructors ************/
+        vector_test()
+        {
+            std::cout << "Test" << std::setw(21) << "Diff" << std::setw(13) << "Time"
+            <<  std::setw(14) << "Capacity" <<  std::setw(8) << "Size" << std::endl;
+        }
+
+        /************ Functions ***************/
+        void Push_back()
+        {
+            START_TIME;
+            for (int i = 0; i < 100; ++i)
+                _vec.push_back(i);
+            _result.time1 = FINISH_TIME;
+
+            START_TIME;
+            for (int i = 0; i < 100; ++i)
+                _ft_vec.push_back(i);
+            _result.time2 = FINISH_TIME;
+
+            print();
+            result("Push_back");
+            _result.clear();
+        }
+
+        void Pop_back()
+        {
+            START_TIME;
+            for (int i = 0; i < 5; ++i)
+                _vec.pop_back();
+            _result.time1 = FINISH_TIME;
+
+            START_TIME;
+            for (int i = 0; i < 5; ++i)
+                _ft_vec.pop_back();
+            _result.time2 = FINISH_TIME;
+
+            print();
+            result("Pop_back");
+            _result.clear();
+        }
+
+        void Assign()
+        {
+            START_TIME;
+            _vec.assign(100, 200);
+            _vec.assign(10, 10);
+            _vec.assign(10, 30);
+            _result.time1 = FINISH_TIME;
+
+            START_TIME;
+            _ft_vec.assign(100, 200);
+            _ft_vec.assign(10, 10);
+            _ft_vec.assign(10, 30);
+            _result.time2 = FINISH_TIME;
+
+            print();
+            result("Assing");
+            _result.clear();
+        }
+
+        void Assign_range()
+        {
+            std::vector<T> temp1;
+            ft::vector<T> temp2;
+            for (int i = 0; i < 500; ++i)
+            {
+                temp1.push_back(i);
+                temp2.push_back(i);
+            }
+
+            START_TIME;
+            _vec.assign(temp1.begin(), temp1.end());
+            _result.time1 = FINISH_TIME;
+
+            START_TIME;
+            _ft_vec.assign(temp2.begin(), temp2.end());
+            _result.time2 = FINISH_TIME;
+
+            print();
+            result("AssingRgn");
+            _result.clear();
+        }
+
+        void Resize()
+        {
+             START_TIME;
+            _vec.resize(_vec.size() + 500);
+            _vec.resize(_vec.size() - 250);
+            _result.time1 = FINISH_TIME;
+
+            START_TIME;
+            _ft_vec.resize(_vec.size() + 500);
+            _ft_vec.resize(_vec.size() - 250);
+            _result.time2 = FINISH_TIME;
+
+            print();
+            result("Resize");
+            _result.clear();
+        }
+
+        void At()
+        {
+            auto test1 = 0;
+            auto test2 = 0;
+
+            START_TIME;
+            test1 = _vec.at(40);
+            _result.time1 = FINISH_TIME;
+
+            START_TIME;
+            test2 = _ft_vec.at(40);
+            _result.time2 = FINISH_TIME;
+
+            if (test1 != test2)
+                _result.error = true;
+            result("At");
+            _result.clear();
+        }
+
+
+
+        
         ~vector_test() {
             log.close();
         }
